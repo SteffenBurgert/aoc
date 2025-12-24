71ff44f473dd2e98a6c2b57bcedbdb7c2f050e62
mohjohfox <marcofuchs@mohjohfox.de>
2025-12-24 11:03:40 +0100

    Add service class to read files from resource dir

---
+++ backend/src/main/kotlin/aoc/backend/service/file/SourceFileExtractor.kt
@@ -0,0 +1,25 @@
+package aoc.backend.service.file
+
+import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.InputStream
+
+@Service
+class SourceFileExtractor {
+
+    fun getResourceFileAsString(fileName: String): String {
+        val inputStream = getResourceFileAsInputStream(fileName)
+        if (inputStream != null) {
+            val bufferReader = BufferedReader(inputStream.reader())
+            return bufferReader.readText()
+        } else {
+           throw IllegalArgumentException("File not found")
+        }
+    }
+
+    fun getResourceFileAsInputStream(fileName: String): InputStream? {
+        val classLoader = this.javaClass.classLoader
+        return classLoader.getResourceAsStream(fileName);
+    }
+
+}
