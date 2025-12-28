b380f754ce825c320246679edfa5067c0c6b6582
SteffenBurgert <steffen.burgert@outlook.de>
2025-12-28 12:48:50 +0100

    aoc-day10-part1: implemented read input and split into parts

--- backend/src/main/kotlin/aoc/backend/service/year/_2025/day10/Day10.kt
+++ backend/src/main/kotlin/aoc/backend/service/year/_2025/day10/Day10.kt
@@ -13,10 +13,33 @@ class Day10() : Day {
     }
 
     override fun part1(lines: List<String>): Long {
+        val input = getInputParts(lines)
+
         return -1L
     }
 
     override fun part2(lines: List<String>): Long {
         return -1L
     }
+
+    private fun getInputParts(lines: List<String>): List<Input> {
+        fun String.parseIntList() =
+            drop(1).dropLast(1).split(',').map(String::toInt)
+
+        return lines.map { line ->
+            line.split(' ').let { parts ->
+                Input(
+                    signal = parts.first().drop(1).dropLast(1),
+                    buttons = parts.drop(1).dropLast(1).map { it.parseIntList() },
+                    times = parts.last().parseIntList()
+                )
+            }
+        }
+    }
 }
+
+data class Input(
+    val signal: String,
+    val buttons: List<List<Int>>,
+    val times: List<Int>,
+)
